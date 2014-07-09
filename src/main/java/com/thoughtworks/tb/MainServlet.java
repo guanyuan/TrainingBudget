cd6110151e99460a6332a09be7d6172416b2d7db
Hongjing Li <hjli@CNhjli.local>
2014-07-09 19:26:36 +0800

    page can show hello, please add your page code

--- src/main/java/com/thoughtworks/tb/MainServlet.java
+++ src/main/java/com/thoughtworks/tb/MainServlet.java
@@ -1,7 +1,35 @@
 package com.thoughtworks.tb;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  * Created by hjli on 7/9/14.
  */
-public class MainServlet {
+public class MainServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        //super.doGet(req, resp);
+        PrintWriter pw  = resp.getWriter();//得到一个输出流
+        //写给Client端一个简单网页信息
+        pw.println("<html><head></head><body>Server:Hello Client~</body></html>");
+        pw.flush();
+        pw.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        super.doPost(req, resp);
+    }
 }
